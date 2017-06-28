104797dfef54702e91b2e63ecbe03c8f7c21ad45
Jean <jean.ellwanger@gmail.com>
2017-06-28 16:12:59 -0300

    Update get-sports-tracker.py
    Melhora no código

--- get-sports-tracker.py
+++ get-sports-tracker.py
@@ -1,33 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import requests
 import os
 from datetime import datetime
 
-i = datetime.now()
+def get_gpx_files(ids_treino):
+	for y, identificador in enumerate(ids_treino):
+		print "Treino: ", y+1 , " de ", len(ids_treino)
+		url = 'http://www.sports-tracker.com/apiserver/v1/workout/exportGpx/'+identificador+'?token='+TOKEN
 
-USUARIO = 'jeeean'
-DATA = i.strftime('%Y-%m-%d')
-NOME_DA_PASTA = 'sports-tracker'
-TOKEN = '24bp4kkk8gbdkf491jfogv1is7e1jd5d'
+		os.system('wget '+url+' -O '+USER+'-'+identificador+'.gpx')
+		os.system('mkdir '+NOME_DA_PASTA)
+		os.system('mv *.gpx '+NOME_DA_PASTA)
+		os.system('zip -r '+NOME_DA_PASTA+'_'+USER+DATA+'.zip '+NOME_DA_PASTA)
+		os.system('rm -r '+NOME_DA_PASTA)
 
-f = open('list.txt', 'r')
-lista = f.readline()
+def get_gpx_ids(STTAuthorization):
+	s = requests.Session()
+	resumo_treinos = s.get('http://www.sports-tracker.com/apiserver/v1/workouts?limited=true&limit=1000000', headers=STTAuthorization).json()
+	resumo_treinos = resumo_treinos['payload']
 
-ids = set([])
+	ids_treino = []
 
-while(lista):
-	expressao_inicio = '<a href="/workout/'
-	inicio = lista.find(expressao_inicio+USUARIO+'/') + len(expressao_inicio)+1 + len(USUARIO)
-	fim = inicio + 24
-	identificador = lista[inicio:fim]
-	if len(identificador) == 24 and identificador.find(' ') == -1 and identificador.find('>') == -1:
-		ids.add(lista[inicio:fim])
-	lista = lista[fim:]
+	for x in resumo_treinos:
+		ids_treino.append(x['workoutKey'])
 
+	return ids_treino
 
-for identificador in ids:
-	url = 'http://www.sports-tracker.com/apiserver/v1/workout/exportGpx/'+identificador+'?token='+TOKEN
-	os.system('wget '+url+' -O '+NOME_DA_PASTA+'-'+identificador+'.gpx')
+if __name__ == '__main__':
 
-os.system('mkdir '+NOME_DA_PASTA)
-os.system('mv *.gpx '+NOME_DA_PASTA)
-os.system('tar -zcvf '+NOME_DA_PASTA+DATA+'.tar.gz '+NOME_DA_PASTA)
-os.system('rm -r '+NOME_DA_PASTA)
+	i = datetime.now()
+	USER = 'jeeean'
+	SENHA = ''
+	NOME_DA_PASTA = 'sports-tracker'
+	DATA = i.strftime('%Y-%m-%d')
+
+	s = requests.Session()
+	login = s.post('http://www.sports-tracker.com/apiserver/v1/login?source=javascript', data={'l': USER, 'p': SENHA})
+
+	TOKEN = login.json()['sessionkey']
+	STTAuthorization = {'STTAuthorization': TOKEN}
+
+	ids = get_gpx_ids(STTAuthorization)
+	get_gpx_files(ids)
